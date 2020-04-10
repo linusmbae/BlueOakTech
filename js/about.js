69cd4b4cf6462330082912105cad21b9b1894b0d
Linus Mutugi <linusmutugi5178@gmail.com>
2020-04-10 21:22:47 +0300

    updated

--- js/about.js
+++ js/about.js
@@ -2,22 +2,22 @@ $(document).ready(function()
 {
   $("#view").click(function()
 {
-  $("#hideToggle").toggle();
-  $("#showToggle").toggle();
+  $("#hideToggle").toggle(800);
+  $("#showToggle").toggle(800);
 });
 $("#view1").click(function()
 {
-  $("#hideToggle").toggle();
-$("#showToggle").toggle();
+  $("#hideToggle").toggle(800);
+$("#showToggle").toggle(800);
 });
 $("#view2").click(function()
 {
-  $("#hideToggle").toggle();
-$("#showToggle").toggle();
+  $("#hideToggle").toggle(800);
+$("#showToggle").toggle(800);
 });
 $("#core").click(function()
 {
-  $("#flipShow").toggle();
-$("#flipHide").toggle();
+  $("#flipShow").toggle(800);
+$("#flipHide").toggle(800);
 });
 });
